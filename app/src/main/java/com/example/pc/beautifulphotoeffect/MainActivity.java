0f10eaf90167d2d742a065e429d97dc68f9f1c40
TAM <vinhtam92@gmail.com>
2015-10-22 23:44:41 +0700

    - home

--- app/src/main/java/com/example/pc/beautifulphotoeffect/MainActivity.java
+++ app/src/main/java/com/example/pc/beautifulphotoeffect/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.pc.beautifulphotoeffect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
     }
 }
